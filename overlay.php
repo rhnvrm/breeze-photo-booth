4cc9a999c59950083ac0890fe37a0d5b55049fb4
rhnvrm <rohanverma2004@gmail.com>
2015-10-14 22:34:40 +0530

    dev cache

--- overlay.php
+++ overlay.php
@@ -12,14 +12,14 @@
 	$path = "cache/".$id.".jpg";
 	$_SESSION['path'] = $path;
 	// only create if not already exists in cache
-	//if($_DEV){
+	if(!$_DEV){
 		if (!file_exists($path)){	
 			create($id, $path);
 		}
 		else{
 			echo " \n already exitst : ".$path;
 		}
-	//}
+	}
 	//override line 13. Always create for testing purposes
 	//create($id, $path);
 		//output as jpeg
