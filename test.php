5e649486a15323234289a46afd7a4dde1a28b428
rhnvrm <rohanverma2004@gmail.com>
2015-10-16 23:45:56 +0530

    fixed transparent area coming in pic and added fb sdk

--- test.php
+++ test.php
@@ -1,10 +1,17 @@
+/<?php 
+
+	require( __DIR__.'/facebook_start.php' ); 
+
+?>
+
 <html>
+
 <head>
   <style>
     body { background:url('images/footer_lodyas.png') repeat }
-    #bg { width: 640px; height: 640px;  font-family: sans-serif; }
-    #fg { position: absolute; width: 640px; height: 640px;  font-family: sans-serif; }
-
+    #bg { width: 640px; height: 640px;   }
+    #fg { position: absolute; width: 640px; height: 640px;   }
+	#container { width: 640px; height: 640px;   }
   </style>
   <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
@@ -38,9 +45,27 @@
    		html2canvas($('#container'), {
 		  onrendered: function(canvas) {
 		  	var data = canvas.toDataURL('image/png');
+		  	//add to dom
 		  	var DOM_img = document.createElement("img");
 		  	DOM_img.src = data;
 		    document.body.appendChild(DOM_img);
+
+		    console.log(data);
+
+		    FB.api(
+			    "/me/photos",
+			    "POST",
+			    {
+			        "url": "{image-url}"
+			    },
+			    function (response) {
+			      if (response && !response.error) {
+			        /* handle the result */
+			      }
+			    }
+			);
+
+
 		  }
 		});
 
@@ -48,11 +73,32 @@
   </script>
 </head>
 <body>
-<div class="container-fluid">
-	<button onclick="enableOverlay()">Display Overlay</button>
-	<button onclick="saveAndSend()">Save</button>
 
-	<div id="container" class="container">
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '<?php echo $_YOUR_APP_ID ?>',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
+<div class="container-fluid">
+
+	<button onclick="enableOverlay()">Display Overlay</button>
+	<button type="submit" onclick="saveAndSend()">Save</button>
+
+
+	<div id="container">
 		<div id="fg" style="background:url('images/temp.png');display: none; z-index: 1"></div>
 		<div id="bg" style="background:url('images/a.jpg'); background-size: cover; z-index: 0"></div>
 	</div>
